04d4dac555e5b026ff3a4de8fe93eb612c3bf9f5
jjpark51 <jjpark57@hotmail.com>
2022-12-02 00:15:50 +0900

    center image try

--- map.js
+++ map.js
@@ -326,8 +326,8 @@ $(function() {
         
         /* 정보창 */
          var infoWindow = new naver.maps.InfoWindow({
-             content: `<div class="infoTab" style="width:200px;text-align:center;padding:10px;border-radius:5px;"><b><div class="imageplace" style="background-image: url(${areaArr[i].imageTag}); background-size: contain; background-repeat: no-repeat;display: block;
-             margin-left: auto; margin-right: auto;"></div>`
+             content: `<div class="infoTab" style="width:200px;text-align:center;padding:10px;border-radius:5px;"><b><center><div class="imageplace" style="background-image: url(${areaArr[i].imageTag}); background-size: contain; background-repeat: no-repeat;display: block;
+             margin-left: auto; margin-right: auto;"></div></center>`
                + areaArr[i].location + '</b><br>' + areaArr[i].description +
                `<br><button class="submit" onclick=listAdd(${i})>Add</button>`
   
